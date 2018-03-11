07ace2e6ec72d85f449a6d32db5de4bd9f98d5b2
Jordi Marqués <jordi@amospalla.es>
2018-03-11 12:20:50 +0100

    update: bashrc.options.local and mbsync.sh

--- mbsync.sh
+++ mbsync.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
-# FileVersion=3
+# FileVersion=4
 
 set -euo pipefail
-_error(){
-	local ec=$?
-	${HOME}/bin/lock unlock "${lock}"
-	exit ${ec}
-}
-
 account="${1}" GPGKEY="${2}"
-
-trap _error ERR SIGINT SIGHUP SIGTERM
-declare lock
-lock="mbsync-${account}"
-if ${HOME}/bin/lock lock -q -f "${lock}"; then
-	export GPGKEY
-	eval $(keychain --noask --agents gpg id_rsa $GPGKEY)
-	mbsync "${account}"
-	${HOME}/bin/program-exists notmuch && grep -q "^path=${HOME}/.Mail/${account}$" "${HOME}/.notmuch-config" && notmuch new || true
-	${HOME}/bin/lock unlock "${lock}"
-else
-	echo "Error: cound not lock '${lock}'."
-	exit 1
-fi
+export GPGKEY
+eval $(keychain --noask --agents gpg id_rsa $GPGKEY)
+mbsync "${account}"
+${HOME}/bin/program-exists notmuch && grep -q "^path=${HOME}/.Mail/${account}$" "${HOME}/.notmuch-config" && notmuch new || true
