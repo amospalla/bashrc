55aa7756da348e14c7456d6030de3deca1127c5f
Jordi Marqués <jordi@amospalla.es>
2018-03-12 10:25:23 +0100

    add osync.run.sh

---
+++ osync.run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export PATH=${PATH}:${HOME}/bin
+eval $(keychain --quiet --agents ssh --eval id_rsa)
+
+profile="${1}"
+exec osync.sh "${HOME}/.osync/${profile}.conf" --verbose
