1a31b27cecc4859692ec173ae28bab5b93e50ac6
Jordi Marqués <jordi@amospalla.es>
2018-03-13 17:18:30 +0100

    osync run.sh output options

--- osync.run.sh
+++ osync.run.sh
@@ -6,4 +6,4 @@ export PATH=${PATH}:${HOME}/bin
 eval $(keychain --quiet --agents ssh --eval id_rsa)
 
 profile="${1}"
-exec osync.sh "${HOME}/.osync/${profile}.conf" --verbose
+exec osync.sh "${HOME}/.osync/${profile}.conf" --errors-only --summary --no-prefix
