bedeec209700c2447de0ddae70bd3178f7d7ec1c
Jordi Marqués <jordi@amospalla.es>
2018-03-14 21:42:55 +0100

    update files

--- mbsync.sh
+++ mbsync.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# FileVersion=4
-
 set -euo pipefail
 account="${1}" GPGKEY="${2}"
 export GPGKEY
-eval $(keychain --noask --agents gpg id_rsa $GPGKEY)
+eval $(keychain --quiet --noask --agents gpg id_rsa $GPGKEY) || exit 1
 mbsync "${account}"
 ${HOME}/bin/program-exists notmuch && grep -q "^path=${HOME}/.Mail/${account}$" "${HOME}/.notmuch-config" && notmuch new || true
--- osync.run.sh
+++ osync.run.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 export PATH=${PATH}:${HOME}/bin
-eval $(keychain --quiet --noask --agents ssh --eval id_rsa)
+eval $(keychain --quiet --noask --agents ssh --eval id_rsa) || exit 1
 
 profile="${1}"
 exec osync.sh "${HOME}/.osync/${profile}.conf" --errors-only --summary --no-prefix
