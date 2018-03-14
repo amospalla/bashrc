21126199d4e81046c1f7a2de51b0750e15d33877
Jordi Marqués <jordi@amospalla.es>
2018-03-14 21:41:12 +0100

    update files

--- osync.run.sh
+++ osync.run.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 export PATH=${PATH}:${HOME}/bin
-eval $(keychain --quiet --agents ssh --eval id_rsa)
+eval $(keychain --quiet --noask --agents ssh --eval id_rsa)
 
 profile="${1}"
 exec osync.sh "${HOME}/.osync/${profile}.conf" --errors-only --summary --no-prefix
