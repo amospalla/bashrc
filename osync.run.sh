acc27d181da0bbddb3e614a144863db8b75ebaf6
Jordi Marqués <jordi@amospalla.es>
2018-03-15 10:37:53 +0100

    update files

--- osync.run.sh
+++ osync.run.sh
@@ -4,6 +4,15 @@ set -euo pipefail
 
 export PATH=${PATH}:${HOME}/bin
 eval $(keychain --quiet --noask --agents ssh --eval id_rsa) || exit 1
-
 profile="${1}"
-exec osync.sh "${HOME}/.osync/${profile}.conf" --errors-only --summary --no-prefix
+
+socket=/tmp/osync.${profile}.socket
+if ! ssh jordi@backup2.amospalla.es -f -N -M -S ${socket} >/dev/null 2>&1; then
+	echo "Error starting ssh tunnel, exit."
+	rm $temp
+	exit 1
+fi
+
+osync.sh "${HOME}/.osync/${profile}.conf" --errors-only --summary --no-prefix
+
+ssh jordi@backup2.amospalla.es -S ${socket} -O exit
