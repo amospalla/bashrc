80ef0fe5bdfdc83557633b90a606acb0c9758efa
Jordi Marqués <jordi@amospalla.es>
2018-03-17 20:35:15 +0100

    update files

--- osync.run.sh
+++ osync.run.sh
@@ -4,20 +4,24 @@ set -euo pipefail
 
 export PATH=${PATH}:${HOME}/bin
 eval $(keychain --quiet --noask --agents ssh --eval id_rsa) || exit 1
-profile="${1}"
+PROFILE="${1}"
+. "${HOME}/.osync/${PROFILE}.conf"
 
-socket=/tmp/osync.${profile}.socket
-if ! ssh jordi@backup2.amospalla.es -f -N -M -S ${socket} >/dev/null 2>&1; then
+if [[ -S /tmp/osync.${PROFILE}.socket ]]; then
+	rm /tmp/osync.${PROFILE}.socket
+fi
+
+socket=/tmp/osync.${PROFILE}.socket
+if ! ssh ${SSH_USERHOST} -p ${SSH_PORT} -f -N -M -S ${socket} >/dev/null 2>&1; then
 	echo "Error starting ssh tunnel, exit."
 	rm $socket
 	exit 1
 fi
 
-. "${HOME}/.osync/${profile}.conf"
 mkdir -p "${INITIATOR_SYNC_DIR}/.sync_dates"
 printf "$(date +"%Y%m%d%H%M%S") $(hostname -f)" > "${INITIATOR_SYNC_DIR}/.sync_dates/$(hostname -f)"
-printf "$(date +"%Y%m%d%H%M%S") ${profile} $(hostname -f)" > "${HOME}/.osync/lastrun.${profile}"
+printf "$(date +"%Y%m%d%H%M%S") ${PROFILE} $(hostname -f)" > "${HOME}/.osync/lastrun.${PROFILE}"
 
-osync.sh "${HOME}/.osync/${profile}.conf" --errors-only --summary --no-prefix
+osync.sh "${HOME}/.osync/${PROFILE}.conf" --errors-only --summary --no-prefix
 
-ssh jordi@backup2.amospalla.es -S ${socket} -O exit
+ssh ${SSH_USERHOST} -p ${SSH_PORT} -S ${socket} -O exit
