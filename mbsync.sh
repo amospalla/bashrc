99ddec7bb310aedade6be1eff0af70ddeb211808
Jordi Marqués <jordi@amospalla.es>
2018-07-09 08:54:01 +0200

    update files

--- mbsync.sh
+++ mbsync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# FileVersion=6
+# FileVersion=8
 
 set -eu -o pipefail -o errtrace
 
@@ -8,11 +8,15 @@ account="${1}"
 hostname="$(hostname -f)"
 
 case "${hostname}" in
-	escriptori.casa.amospalla.es) export GPGKEY=0951EEA0 ;;
-	  portatil.casa.amospalla.es) export GPGKEY=2309924E ;;
-	                           *) echo "Error: no GPGKEY defined in $(readlink -f "${0}") for hostname '${hostname}'."; exit 1 ;;
+	     escriptori.casa.amospalla.es) export GPGKEY=0951EEA0 ;;
+	       portatil.casa.amospalla.es) export GPGKEY=2309924E ;;
+	jordimarques.desktop.minorisa.net) export GPGKEY=2309924E ;;
+	                                *) echo "Error: no GPGKEY defined in $(readlink -f "${0}") for hostname '${hostname}'."; exit 1 ;;
 esac
 
+lock="$(echo "${account}" | sed 's/-[^@.]*$//')"
+lock="${lock//./-}"
+
 eval $(keychain --quiet --noask --agents gpg id_rsa $GPGKEY) || exit 1
-lock lock -q -f noerror mbsync-jordi@amospalla.es mbsync "${account}"
+lock lock -q -f noerror mbsync-${lock} mbsync "${account}"
 ${HOME}/bin/program-exists -q notmuch && grep -q "^path=${HOME}/.Mail/${account}$" "${HOME}/.notmuch-config" && notmuch new || true
