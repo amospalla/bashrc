f6814ed8ea68aedd6ca36844dcbde44040a7ea7a
Jordi Marqués <jordi@amospalla.es>
2018-07-21 11:21:43 +0200

    update files

--- local-install.sh
+++ local-install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# FileVersion=10
+# FileVersion=12
 
 # TODO
-# recomplilar neomutt amb libnotmuch-dev
 # afegir arquitectura
 
 set -eu -o pipefail -o errtrace
@@ -274,11 +273,11 @@ declare -a online_available=() installed=() packages_status=()
 
 # Required packages: install and left installed
 # Optional packages: install and remove after package installation
-vars["neomutt_Ubuntu_16.04_packages_required"]="libc6 libgnutls30 libgpgme11 libgssapi-krb5-2 libidn11 liblua5.3-0 libncursesw5 libnotmuch4 libsasl2-2 libtinfo5 libtokyocabinet9 mime-support"
-vars["neomutt_Ubuntu_16.04_packages_dev"]="build-essential git gettext xsltproc libxml2-utils docbook-xml docbook-xsl libncursesw5-dev libidn11-dev"
+vars["neomutt_Ubuntu_16.04_packages_required"]="libc6 libgnutls30 libgpgme11 libgssapi-krb5-2 libidn11 libncursesw5 libnotmuch4 libsasl2-2 libtinfo5 libtokyocabinet9 mime-support"
+vars["neomutt_Ubuntu_16.04_packages_dev"]="build-essential git gettext xsltproc libxml2-utils docbook-xml docbook-xsl libncursesw5-dev libidn11-dev libgnutls28-dev libkrb5-dev libtokyocabinet-dev libnotmuch-dev libgpgme11-dev"
 vars["neomutt_Ubuntu_16.04_git"]="https://github.com/neomutt/neomutt"
 vars["neomutt_Ubuntu_16.04_url"]=""
-vars["neomutt_Ubuntu_16.04_configure_command"]="./configure --prefix=/usr/local"
+vars["neomutt_Ubuntu_16.04_configure_command"]="./configure --prefix=/usr/local --sasl --notmuch --tokyocabinet --gss --gpgme --gnutls"
 vars["neomutt_Ubuntu_16.04_configure_path"]=""
 vars["neomutt_Ubuntu_16.04_git_branch"]=""
 vars["neomutt_Ubuntu_16.04_git_recursive"]=""
@@ -294,11 +293,11 @@ vars["polybar_Ubuntu_16.04_git_branch"]="3.1.0"
 vars["polybar_Ubuntu_16.04_git_recursive"]="1"
 vars["polybar_Ubuntu_16.04_install_path"]="/usr/local"
 
-vars["neomutt_Ubuntu_18.04_packages_required"]="libc6 libgnutls30 libgpgme11 libgssapi-krb5-2 libidn11 liblua5.3-0 libncursesw5 libnotmuch5 libsasl2-2 libtinfo5 libtokyocabinet9 mime-support"
-vars["neomutt_Ubuntu_18.04_packages_dev"]="build-essential git gettext xsltproc libxml2-utils docbook-xml docbook-xsl libncursesw5-dev libidn11-dev"
+vars["neomutt_Ubuntu_18.04_packages_required"]="libc6 libgnutls30 libgpgme11 libgssapi-krb5-2 libidn11 libncursesw5 libnotmuch5 libsasl2-2 libtinfo5 libtokyocabinet9 mime-support"
+vars["neomutt_Ubuntu_18.04_packages_dev"]="build-essential git gettext xsltproc libxml2-utils docbook-xml docbook-xsl libncursesw5-dev libidn11-dev libgnutls28-dev libkrb5-dev libtokyocabinet-dev libnotmuch-dev libgpgme-dev"
 vars["neomutt_Ubuntu_18.04_git"]="https://github.com/neomutt/neomutt"
 vars["neomutt_Ubuntu_18.04_url"]=""
-vars["neomutt_Ubuntu_18.04_configure_command"]="./configure --prefix=/usr/local"
+vars["neomutt_Ubuntu_18.04_configure_command"]="./configure --prefix=/usr/local --sasl --notmuch --tokyocabinet --gss --gpgme --gnutls"
 vars["neomutt_Ubuntu_18.04_configure_path"]=""
 vars["neomutt_Ubuntu_18.04_git_branch"]=""
 vars["neomutt_Ubuntu_18.04_git_recursive"]=""
